4f9bc61b29ca6a8ce01731cb99128750450b35ca
Dhruv Pratap Singh <dhruvpratapsinghmer@gmail.com>
2022-09-26 22:28:40 +0530

    lol

---
+++ React/my_app/build/precache-manifest.fcce5c8d2b6bf65ab8fc70ce4562a6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "613ec49f1a74e8f05a52",
+    "url": "/TEXTUTILS/static/css/main.1292ceb9.chunk.css"
+  },
+  {
+    "revision": "613ec49f1a74e8f05a52",
+    "url": "/TEXTUTILS/static/js/main.970a638e.chunk.js"
+  },
+  {
+    "revision": "2c2924f91f541c1c8f8f",
+    "url": "/TEXTUTILS/static/js/runtime~main.cfa8aa45.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/TEXTUTILS/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/TEXTUTILS/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "bfc5eba0859b2b374f790737a2774b73",
+    "url": "/TEXTUTILS/index.html"
+  }
+];
